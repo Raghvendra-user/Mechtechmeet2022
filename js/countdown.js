5e5cc9feb28f376fc97eeea5b9600e9fdb6401bb
Raghvendra Singh Parihar <63204116+Raghvendra-user@users.noreply.github.com>
2022-04-22 23:16:19 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -33,7 +33,7 @@
 
             //do something later when date is reached
             if (distance < 0) {
-                document.getElementById("headline").innerText = "MTM-X has been ended successfully !!";
+                document.getElementById("headline").innerText = "MTM-X has concluded successfully !!";
                 document.getElementById("countdown").style.display = "none";
                 // document.getElementById("content").style.display = "block";
                 clearInterval(x);
